46142a254705af5b6b7fd543ea05e983ee80f352
happerli <happerli@github.com>
2016-04-18 10:04:51 +0800

    add ignored files.

--- .emacs.d/init/init-ignored.el
+++ .emacs.d/init/init-ignored.el
@@ -1,11 +1,11 @@
 (require 'projectile)
 
-(defcustom my-ignored-directories (list ".git" ".svn" ".hg" "GeneratedFiles" "buildpdb" "lib" "sln" "log" "bin" "build" "build32" "build64" "build-x64" "build-x86" "build-64" "build-32" "build-vc11" "build-vc11-32" "build-vc11-64" "build-vc11-x86" "build-vc11-x64" ".backups" "debug" "release")
+(defcustom my-ignored-directories (list ".git" ".svn" ".hg" "GeneratedFiles" "buildpdb" "lib" "sln" "log" "bin" "build" "build32" "build64" "build-x64" "build-x86" "build-64" "build-32" "build-vc11" "build-vc11-32" "build-vc11-64" "build-vc11-x86" "build-vc11-x64" ".backups" "debug" "release" "google")
   "my-ignored-directories"
   :type 'list
   :group 'my-ignored)
 
-(defcustom my-ignored-files (list "GPATH" "GRTAGS" "GTAGS" "TAGS" ".*" ".css" "*.log" "*.db" "#*#" "*~*" "*.exe" "*.png" "*.jpg" "*.bmp" "*.filters" "*.user" "*.gif" "*.pdb" "*.dll" "*.pb.cc")
+(defcustom my-ignored-files (list "GPATH" "GRTAGS" "GTAGS" "TAGS" ".*" ".css" "*.log" "*.db" "#*#" "*~*" "*.exe" "*.png" "*.jpg" "*.bmp" "*.filters" "*.user" "*.gif" "*.pdb" "*.dll" "*.pb.cc" "*.pb.h" "*.zip" "*.rar")
   "my-ignored-files"
   :type 'list
   :group 'my-ignored)

7f3aa4a911fc8b38fd0cf15b36e33f9ff7391c94
happerli <happerli@github.com>
2017-04-18 17:16:19 +0800

    custom my cpp style from google style.

--- .emacs.d/init/init-cc-mode.el
+++ .emacs.d/init/init-cc-mode.el
@@ -1,51 +1,62 @@
-;C/C++ SECTION
-  ; @see https://github.com/seanfisk/cmake-flymake
-  ; make sure you project use cmake
- ;;;(flymake-mode 1)
- ;;;(if (and buffer-file-name
- ;;;         (not (or (string-match "^/usr/local/include/.*" buffer-file-name)
- ;;;                  (string-match "^/usr/src/linux/include/.*" buffer-file-name))
- ;;;              ))
- ;;;    (cppcm-reload-all))
-
-(defun my-c-mode-hook ()
-  (setq c-default-style "linux")
-  (setq default-tab-width 4)
-  (setq c-basic-offset 4)
-
-  ;syntax-highlight aggressively
-  ;(setq font-lock-support-mode 'lazy-lock-mode)
-  (setq lazy-lock-defer-contextually t)
-  (setq lazy-lock-defer-time 0)
-
-  ;make DEL take all previous whitespace with it
-  (c-toggle-hungry-state 1)
-
-  (require 'fic-mode)
-  (add-hook 'c++-mode-hook 'turn-on-fic-mode)
+;;;C/C++ SECTION
+(require 'cc-mode)
+(defun emacs-format-function ()
+  "Format the whole buffer."
+  (c-set-style "google")
+  (indent-region (point-min) (point-max) nil)
+  (untabify (point-min) (point-max))
   )
-
-;; donot use c-mode-common-hook or cc-mode-hook because many major-modes use this hook
-(add-hook 'c-mode-common-hook
-          (lambda ()
-            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
-              ;; indent
-              ;; (fix-c-indent-offset-according-to-syntax-context 'substatement 0)
-              ;; (fix-c-indent-offset-according-to-syntax-context 'func-decl-cont 0)
-
-              ;; gtags (GNU global) stuff
-              (setq gtags-suggested-key-mapping t)
-              (if *emacs24* (ggtags-mode 1)))
-            (when (derived-mode-p 'c-mode 'c++-mode)
-              (my-c-mode-hook))
-            ))
 
 ;; for modern-c++-font-lock-mode
 (add-hook 'c++-mode-hook #'modern-c++-font-lock-mode)
 (add-hook 'c-mode-hook #'modern-c++-font-lock-mode)
 
-(require 'google-c-style)
-(add-hook 'c-mode-common-hook 'google-set-c-style)
-(add-hook 'c-mode-common-hook 'google-make-newline-indent)
+;;Create MyCppStyle
+(defconst MyCppStyle
+  '((c-tab-always-indent . t)
+    (c-offsets-alist . ((substatement-open . 0)
+                        (innamespace . 0)
+                        (inline-open . 0)
+                        (block-open     . 0)))
+    ;;(c-echo-syntactic-information-p t)
+    ;;(setq indent-tabs-mode nil)
+    )
+  "My Cpp Coding Style")
+(c-add-style "MyCppStyle" MyCppStyle)
+
+;;Define own hook
+(defun MyCppHook ()
+  (setq c-style-variables-are-local-p nil)
+  (require 'google-c-style)
+  (add-hook 'c-mode-common-hook 'google-set-c-style)
+  (add-hook 'c-mode-common-hook 'google-make-newline-indent)
+  (setq c-default-style "google")
+  (c-set-style "MyCppStyle")
+
+  (setq indent-tabs-mode nil)
+  (setq default-tab-width 4)
+  (setq c-basic-offset 4)
+  (setq c-tab-always-indent t)
+  (setq c++-tab-always-indent t)
+
+  (setq lazy-lock-defer-contextually t)
+  (setq lazy-lock-defer-time 0)
+
+  ;;;make DEL take all previous whitespace with it
+  (c-toggle-hungry-state 1)
+
+  (require 'fic-mode)
+  (add-hook 'c++-mode-hook 'turn-on-fic-mode)
+  )
+(add-hook 'c++-mode-hook 'MyCppHook)
+(add-hook 'c-mode-hook 'MyCppHook)
+
+;;set *.h and *.c and *.cpp files use c++ mode
+(setq auto-mode-alist
+      (cons '("\\.h$" . c++-mode) auto-mode-alist))
+(setq auto-mode-alist
+      (cons '("\\.c$" . c++-mode) auto-mode-alist))
+(setq auto-mode-alist
+      (cons '("\\.cpp$" . c++-mode) auto-mode-alist))
 
 (provide 'init-cc-mode)
